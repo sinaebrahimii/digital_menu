00cf01cb7c33d66b30e3de957436dc564543c374
SinEebrahimi <sinaebrahimi31@gmail.com>
2024-04-24 20:58:25 +0330

    Put request created to update the items

--- routers/items.py
+++ routers/items.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException,Depends,Path
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from typing import Annotated,Union
 from starlette import status
@@ -23,16 +25,37 @@ class ItemRequest(BaseModel):
     image_url:Union[str,None]=None
     category_id:int
 
+class ItemResponse(BaseModel):
+    id:Union[int,None]=None
+    name:str
+    price:int
+    available:bool
+    image_url:Union[str,None]=None
+    category_id:int
 @router.get("/{category_id}",status_code=status.HTTP_200_OK)
-def read_items(db:db_dependency, category_id:int=Path(gt=0)):
-    items=db.query(Item).filter(Category.id==category_id).all()
+async def read_items(db:db_dependency, category_id:int=Path(gt=0)):
+    items=db.query(Item).filter(Item.category_id==category_id).all()
     if items is not None:
        return  items
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 @router.post("/",status_code=status.HTTP_201_CREATED)
-def create_item(db:db_dependency, item_request:ItemRequest):
+async def create_item(db:db_dependency, item_request:ItemRequest):
     item=Item(**item_request.model_dump())
     db.add(item)
     db.commit()
+
+@router.put("/{item_id}",status_code=status.HTTP_200_OK,response_model=ItemResponse)
+async def update_item(db:db_dependency, item_request:ItemRequest,item_id:int=Path(gt=0)):
+    item=db.query(Item).filter(Item.id==item_id).first()
+    if item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    item.name=item_request.name
+    item.price=item_request.price
+    item.available=item_request.available
+    item.image_url=item_request.image_url
+    item.category_id=item_request.category_id
+    db.add(item)
+    db.commit()
+    return item
 
