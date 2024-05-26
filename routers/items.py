819e6c9c53b052b3102ffded883d70d2b5fb9d3c
SinEebrahimi <sinaebrahimi31@gmail.com>
2024-05-26 23:34:54 +0330

    item api

--- routers/items.py
+++ routers/items.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from typing import Annotated,Union
 from starlette import status
-from models import Item,Category
+from models import Item,Category,Option
 from database import SessionLocal
 from pydantic import BaseModel,Field
 
@@ -25,6 +25,10 @@ class ItemRequest(BaseModel):
     image_url:Union[str,None]=None
     category_id:int
 
+class OptionRequest(BaseModel):
+    name:str
+    price:int
+
 class ItemResponse(BaseModel):
     id:Union[int,None]=None
     name:str
@@ -32,9 +36,12 @@ class ItemResponse(BaseModel):
     available:bool
     image_url:Union[str,None]=None
     category_id:int
+
 @router.get("/{category_id}",status_code=status.HTTP_200_OK)
 async def read_items(db:db_dependency, category_id:int=Path(gt=0)):
     items=db.query(Item).filter(Item.category_id==category_id).all()
+    for i in items:
+        print (i.options)
     if items is not None:
        return  items
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
@@ -43,6 +50,12 @@ async def read_items(db:db_dependency, category_id:int=Path(gt=0)):
 async def create_item(db:db_dependency, item_request:ItemRequest):
     item=Item(**item_request.model_dump())
     db.add(item)
+    db.commit()
+
+@router.post("/options/{item_id}",status_code=status.HTTP_201_CREATED)
+async def create_options(db:db_dependency,option_request:OptionRequest,item_id:int=Path(gt=0)):
+    option=Option(name=option_request.name,price=option_request.price,item_id=item_id)
+    db.add(option)
     db.commit()
 
 @router.put("/{item_id}",status_code=status.HTTP_200_OK,response_model=ItemResponse)
@@ -59,3 +72,11 @@ async def update_item(db:db_dependency, item_request:ItemRequest,item_id:int=Pat
     db.commit()
     return item
 
+@router.delete('/{item_id}',status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(db:db_dependency,item_id:int=Path(gt=0)):
+    item=db.query(Item).filter(Item.id==item_id).first()
+    if item is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    db.query(Item).filter(Item.id==item_id).delete()
+    db.commit()
+    
