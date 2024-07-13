7391c9bf2c359907b6268aaed9e097f082b3199b
sepehrebrahimii <sepehrebrahimi82@gmail.com>
2024-07-13 15:46:53 +0330

    small changes (cascade delete option)

--- models.py
+++ models.py
@@ -3,41 +3,6 @@ from sqlalchemy import Column , Integer, String,Boolean,ForeignKey
 from sqlalchemy.orm import relationship,Mapped,mapped_column
 from typing import List
 
-# class Item(Base):
-#     __tablename__ = 'items'
-#     id:Mapped[int]=mapped_column(primary_key=True,index=True)
-#     name:Mapped[str]
-#     price:Mapped[int]
-#     available:Mapped[bool]=mapped_column(default=True)
-#     image_url:Mapped[str]
-#     category_id:Mapped[int]=mapped_column(ForeignKey('categories.id'))
-#     options: Mapped[List["Option"]] = relationship(
-#         back_populates="item", cascade="all, delete-orphan"
-#     )
-#     category:Mapped["Category"]=relationship(back_populates="items")
-#
-#
-# class Option(Base):
-#     __tablename__ = 'options'
-#     id:Mapped[int]=mapped_column(primary_key=True,index=True,)
-#     name:Mapped[int]
-#     price:Mapped[int]
-#     item_id:Mapped[int]=mapped_column(ForeignKey('items.id'))
-#     item:Mapped["Item"]=relationship(back_populates="options")
-#
-# class Category(Base):
-#     __tablename__ = 'categories'
-#     id:Mapped[int]=mapped_column(primary_key=True,index=True)
-#     name:Mapped[str]
-#     items:Mapped[List["Item"]]=relationship( back_populates="category",cascade="all, delete-orphan")
-#
-# class User(Base):
-#     __tablename__ = 'users'
-#     id:Mapped[int]=mapped_column(primary_key=True,index=True)
-#     email:Mapped[str]=mapped_column(unique=True)
-#     role:Mapped[str]
-#     password:Mapped[str]
-
 class Item(Base):
     __tablename__ = 'items'
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
@@ -45,9 +10,9 @@ class Item(Base):
     price: Mapped[int]
     available: Mapped[bool] = mapped_column(default=True)
     image_url: Mapped[str]
-    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id',))
     options: Mapped[List["Option"]] = relationship(
-        back_populates="item", cascade="all, delete-orphan"
+        back_populates="item", cascade="all"
     )
     category: Mapped["Category"] = relationship(back_populates="items")
 
@@ -57,7 +22,7 @@ class Option(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str]
     price: Mapped[int]
-    item_id: Mapped[int] = mapped_column(ForeignKey('items.id'),)
+    item_id: Mapped[int] = mapped_column(ForeignKey('items.id',ondelete="CASCADE"),)
     item: Mapped["Item"] = relationship(back_populates="options",)
 
 class Category(Base):
